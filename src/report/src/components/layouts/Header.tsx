9606553fc1711a3e061898df541f96741103447a
Arthur Silva | JC2Sec <arthur.silva@jc2sec.com.br>
2026-04-30 14:52:08 -0300

    report: header and logo changes

--- src/report/src/components/layouts/Header.tsx
+++ src/report/src/components/layouts/Header.tsx
@@ -28,9 +28,9 @@ export function Header() {
     console.log(reportData);
     return (
         <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur">
-            <div className="container px-4 md:px-8 flex h-14 items-center">
+            <div className="container px-4 md:px-8 flex h-20 items-center">
                 <div className="mr-4 hidden md:flex">
-                    <NavLink to="/" className="mr-6 flex items-center space-x-2">
+                    <NavLink to="/" className="mr-12 flex items-center space-x-2">
                         <Logo />
                     </NavLink>
                     <nav className="flex items-center space-x-6 text-sm font-medium">
@@ -88,7 +88,7 @@ export function Header() {
                             variant="ghost"
                             className="mr-4 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
                             <ViewVerticalIcon className="h-5 w-5" />
-                            <span className="sr-only">Toggle Menu</span>
+                            <span className="sr-only">Menu</span>
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left" className="pr-0 sm:max-w-xs">
--- src/report/src/components/logo.tsx
+++ src/report/src/components/logo.tsx
@@ -1,11 +1,18 @@
 import { ztAppConfig } from "@/config/app";
 import { Icons } from "./icons";
+import jc2Logo from "../assets/jc2sec.png";
 
 export function Logo() {
     return (
         <>
-            <Icons.logo className="h-6 w-6" />
-            <span className="font-bold">{ztAppConfig.name}</span>
+            <Icons.logo className="h-10 w-10" />
+            <div className="flex flex-col">
+                <p className="font-bold">{ztAppConfig.name}</p>
+                <p className="font-light text-xs text-muted-foreground">
+                    boosted by <img src={jc2Logo} alt="JC2SEC" className="inline h-4" />
+                </p>
+            </div>
+
         </>
     )
 }
